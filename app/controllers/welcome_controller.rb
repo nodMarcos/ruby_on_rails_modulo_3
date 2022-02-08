9598e17244740b637a67b3838e8ba0e1faa324b3
Marcos <nodmarcos@gmail.com>
2022-02-08 00:30:25 -0300

    viewed: cookies and sessions, Asset Pipeline, pre-compilator and tasks to pre-compile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires [COOKIE]"
+    session[:curso] = "Marquin Dj [SESSION]"
     @nome = params[:nome]
     @curso = params[:curso]
   end
